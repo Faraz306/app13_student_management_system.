7ba7b8063fefece35dc1faaf6a144e69a5c29ae5
Faraz <farazmoin.uk@gmail.com>
2026-01-24 07:50:20 -0500

    Added records in Table.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel, QGridLayout, QLineEdit, QPushButton, \
-    QMainWindow, QTableWidget
+    QMainWindow, QTableWidget, QTableWidgetItem, QAbstractItemView
 
 import sys
+
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,12 +24,22 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget(self)
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def data_table(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
 
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.data_table()
 sys.exit(app.exec())
